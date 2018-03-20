cec15dc8c2127868dc25dfb84f2cb31ea4b2763b
rmarganti <ryanmarganti@soulsizzle.com>
2018-03-20 13:49:22 -0400

    F replace supafetch with axios (#4)
    * Replaces internal fetch implementation with axois.

--- src/redux/middleware.ts
+++ src/redux/middleware.ts
@@ -1,3 +1,5 @@
+import axios, { AxiosError } from 'axios';
+import { path } from 'ramda';
 import { ActionCreator, Dispatch, Middleware, Store } from 'redux';
 import {
     iAttributes,
@@ -5,6 +7,7 @@ import {
     iJsonApiResponseWithData,
     iResourceObject,
     JsonApiResponse,
+    iJsonApiResponseWithError,
 } from 'ts-json-api';
 
 import ResourceObject from 'ts-json-api/dist/ResourceObject';
@@ -21,12 +24,13 @@ import {
 } from './actions';
 
 import { JASON_API_REQUEST } from '../constants';
-import * as network from '../utils/fetch';
 import {
     iTransformer,
     iSuccessCallback,
     iJsonApiActionConfig,
 } from '../interfaces/Middleware';
+
+import { extractJsonApiErrorFromAxios, stringifyJsonApiErrors } from '../utils/async';
 
 export interface MiddlewareConfig {
     startLoadingActionCreator?: ActionCreator<any>;
@@ -44,28 +48,20 @@ class JsonApiMiddleware {
 
     private action: iJsonApiActionConfig;
 
-    private resourceType: string;
+    private resourceType?: string;
 
-    private resourceId: string;
+    private resourceId?: string;
 
     private store: Store<any>;
 
-    constructor(config: MiddlewareConfig = {}) {
-        this.config = config;
-    }
-
-    /**
-     * Intercept `JASON_API_REQUEST` type actions
-     * and let others bubble through
-     */
-    public middleware(
+    constructor(
+        config: MiddlewareConfig = {},
         store: Store<any>,
         next: Dispatch<any>,
         action: iJsonApiActionConfig
     ) {
-        if (action.type !== JASON_API_REQUEST) {
-            return next(<any>action);
-        }
+        this.config = config;
+        this.store = store;
 
         this.action = Object.assign(
             {},
@@ -76,27 +72,32 @@ class JsonApiMiddleware {
             },
             action
         );
+    }
 
-        this.store = store;
-
+    /**
+     * Intercept `JASON_API_REQUEST` type actions
+     * and let others bubble through
+     */
+    public executeMiddleware() {
         if (
             !this.action.disableStartLoadingActionCreator &&
             this.config.startLoadingActionCreator
         ) {
-            store.dispatch(this.config.startLoadingActionCreator());
+            this.store.dispatch(this.config.startLoadingActionCreator());
         }
 
-        const payload: Payload =
+        const payload: Payload | undefined =
             this.action.payload instanceof ResourceObject
-                ? { data: <ResourceObject>this.action.payload.toJSON() }
+                ? this.action.payload && { data: this.action.payload.toJSON() }
                 : this.action.payload;
 
         this.resourceType =
             this.action.resourceType ||
-            (payload && payload.data && payload.data.type);
+            path(['data', 'type'], payload);
+
         this.resourceId =
             this.action.resourceId ||
-            (payload && payload.data && payload.data.id);
+            path(['data', 'id'], payload);
 
         this.setLoadingMeta();
         this.clearError();
@@ -109,20 +110,16 @@ class JsonApiMiddleware {
      *
      * @param payload
      */
-    private async executeRequest(payload: Payload) {
-        const method = this.action.method || 'get';
-        const networkCall = network.request(
-            method,
-            this.action.url,
-            payload,
-            this.action.additionalHeaders
-        );
+    private async executeRequest(payload?: Payload) {
+        const networkCall = this.buildNetworkCall(payload);
 
         try {
             const response = await networkCall;
-            const transformedResponse = this.action.transformer
-                ? this.action.transformer.call(null, response)
-                : response;
+            const data = response.data;
+
+            const transformedData = this.action.transformer
+                ? this.action.transformer.call(null, data)
+                : data;
 
             if (
                 !this.action.disableStartLoadingActionCreator &&
@@ -132,15 +129,17 @@ class JsonApiMiddleware {
             }
 
             this.clearLoadingMeta();
-            this.finishLoading(transformedResponse);
-            this.executeOnSuccessActions(transformedResponse);
+            this.finishLoading(transformedData);
+            this.executeOnSuccessActions(transformedData);
 
             if (this.action.onSuccess) {
-                this.action.onSuccess(transformedResponse);
+                this.action.onSuccess(transformedData);
             }
 
-            return transformedResponse;
+            return transformedData;
         } catch (error) {
+            const errorJson = extractJsonApiErrorFromAxios(error);
+
             if (
                 !this.action.disableStartLoadingActionCreator &&
                 this.config.stopLoadingActionCreator
@@ -158,19 +157,36 @@ class JsonApiMiddleware {
                     this.config.authenticationExpiredActionCreator(error)
                 );
             } else {
-                this.handleError(error);
+                this.handleError(errorJson);
                 if (
                     this.action.displayNotificationOnError &&
                     this.config.displayErrorActionCreator
                 ) {
                     this.store.dispatch(
-                        this.config.displayErrorActionCreator(error.message)
+                        this.config.displayErrorActionCreator(
+                            stringifyJsonApiErrors(errorJson)
+                        )
                     );
                 }
             }
 
-            throw error;
+            console.log('asdf', errorJson);
+
+            throw errorJson;
         }
+    }
+
+    private buildNetworkCall(payload?: Payload) {
+        return axios.request({
+            data: payload,
+            headers: {
+                Accept: 'application/vnd.api+json',
+                ContentType: !(payload instanceof FormData) && 'application/vnd.api+json',
+                ...this.action.additionalHeaders,
+            },
+            method: this.action.method,
+            url: this.action.url,
+        });
     }
 
     /**
@@ -339,35 +355,35 @@ class JsonApiMiddleware {
      *
      * @param error
      */
-    private handleError(error: any) {
+    private handleError(errorBody: iJsonApiResponseWithError) {
+        if (!this.resourceType) {
+            return;
+        }
+
         if (this.resourceId) {
             this.store.dispatch(
                 updateResourceObjectMeta(
                     this.resourceType,
                     this.resourceId,
                     'errors',
-                    error.errors
+                    errorBody.errors
                 )
             );
             return;
         }
 
         this.store.dispatch(
-            updateResourceObjectsMeta(this.resourceType, 'errors', error)
+            updateResourceObjectsMeta(this.resourceType, 'errors', errorBody)
         );
     }
 
     /**
      * Check to see if the provide error is an Authentication expired error
      *
-     * @param possibleError
+     * @param error
      */
-    private checkForAuthenticationError(possibleErrors: any): boolean {
-        if (!Array.isArray(possibleErrors)) {
-            return false;
-        }
-
-        return !!possibleErrors.find(error => error.status === 401);
+    private checkForAuthenticationError(error: AxiosError): boolean {
+        return error.response && error.response.status === 401 || false;
     }
 }
 
@@ -380,8 +396,14 @@ export const middlewareFactory = (config: MiddlewareConfig = {}): any => {
     return <Middleware>(store: Store<any>) => (next: Dispatch<any>) => (
         action: iJsonApiActionConfig
     ) => {
-        const jsonApiMiddleware = new JsonApiMiddleware(config);
-        return jsonApiMiddleware.middleware(store, next, action);
+        if (action.type !== JASON_API_REQUEST) {
+            return next(<any>action);
+        }
+
+        const jsonApiMiddleware =
+            new JsonApiMiddleware(config, store, next, action);
+
+        return jsonApiMiddleware.executeMiddleware();
     };
 };
 
---
+++ src/utils/async.ts
@@ -0,0 +1,33 @@
+import { AxiosError } from "axios";
+import { iJsonApiResponseWithError } from "ts-json-api";
+
+/**
+ * Extract valid JSON API errors from an Axios error response. If the API
+ * did not return valid JSON API response, build a generic one that matches
+ * the format.
+ *
+ * @param error
+ */
+export const extractJsonApiErrorFromAxios =
+    (error: AxiosError): iJsonApiResponseWithError => {
+        if (error.response && error.response.data.errors) {
+            return error.response.data;
+        } else if (error.request) {
+            return {
+                errors: [{
+                    status: error.request.status,
+                    detail: error.request.statusText
+                }],
+            };
+        }
+
+        return {
+            errors: [{
+                detail: error.message,
+            }],
+        };
+    }
+
+
+export const stringifyJsonApiErrors = (errorJson: iJsonApiResponseWithError) =>
+    errorJson.errors.map(error => error.detail || error.title).join('\n');
