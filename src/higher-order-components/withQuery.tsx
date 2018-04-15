0b0a78b8fbb937da1e8b44331ef56cb7e117df5c
Ryan Marganti <rmarganti@gannett.com>
2018-04-15 12:08:36 -0400

    Improved Omit type with Typescript 2.8

--- src/higher-order-components/withQuery.tsx
+++ src/higher-order-components/withQuery.tsx
@@ -8,10 +8,7 @@ import { cacheQuery } from '../redux/actions';
 import { getCachedQuery } from '../redux/selectors';
 import { hashObject, simplifyJsonApi } from '../utils/data';
 
-type Diff<T extends string, U extends string> = ({ [P in T]: P } &
-    { [P in U]: never } & { [x: string]: never })[T];
-
-type Omit<T, K extends keyof T> = Pick<T, Diff<keyof T, K>>;
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 
 export type QueryFactory = (
     dispatch: JasonApiDispatch,
@@ -56,7 +53,7 @@ const withQuery = ({
     propsToWatch = [],
     stateBranch = 'resourceObjects',
 }: WithQueryOptions) => <OriginalProps extends WithQueryInjectedProps>(
-    BaseComponent: React.ComponentType<OriginalProps & WithQueryInjectedProps>
+    BaseComponent: React.ComponentType<OriginalProps>
 ) => {
     type ExternalProps = Omit<OriginalProps, keyof WithQueryInjectedProps>;
     type InternalProps = ExternalProps & ConnectedProps;
